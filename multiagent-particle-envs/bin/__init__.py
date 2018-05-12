4554251f81956a206507b6afd44e1c786562d943
Luka Zivanovic <lukazivanovic1995@gmail.com>
2018-05-12 17:57:02 +0200

    ...

---
+++ multiagent-particle-envs/bin/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os,sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import argparse
+
+from multiagent.environment import MultiAgentEnv
+from multiagent.policy import InteractivePolicy
+import multiagent.scenarios as scenarios
+import numpy as np
+
+if __name__ == '__main__':
+    # parse arguments
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('-s', '--scenario', default='simple.py', help='Path of the scenario Python script.')
+    args = parser.parse_args()
